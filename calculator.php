23cc80fde709cff240c06aa8ffcfb1d9b793273a
willy1669 <williamschinonso11@gmail.com>
2020-01-21 11:36:28 +0100

    modifications

--- calculator.php
+++ calculator.php
@@ -8,11 +8,38 @@
 </head>
 <body>
     <form action="site.php" method="get">
-        <h1>Addition</h1>
-   <input type="number" name="num1"><br />
-   <input type="number" name="num2"><br />
-   <input type="submit">
+    
     </form>
- Answer: <?php echo $_GET["num1"] + $_GET["num2"];?>
+<?php 
+    // looping through arrays in php
+    $luckyNames = array("oluwaloni", "richard", "sodeinde");
+    for($i = 0; $i < count($luckyNames); $i++){
+        echo "$luckyNames[2] <br>";
+    }
+    // include allows us to use another file in our php file which is synonymous to components in react.
+    include "header.html";
+    // classes and objects in php
+    class Book{
+        var $title;
+        var $author;
+        var $pages;
+
+        // constructors
+        function _construct(){
+            echo "the books"
+        }
+    }
+
+    $book1 = new Book;
+    $book1 ->title = "Harry Potter";
+    $book1 ->author = "JK Rowling";
+    $book1 ->pages = 400;
+
+    $book2 = new Book;
+    $book2 ->title = "Lord of the rings";
+    $book2 ->author = "Tolkin";
+    $book2 ->pages = 700;
+
+?>
 </body>
 </html>
